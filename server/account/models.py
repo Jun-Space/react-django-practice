90f7db535630c56d862e72bcebb3f79a881464b8
Jun0922 <tjrwns126@gmail.com>
2020-12-23 23:05:00 +0900

    initial commit

---
+++ server/account/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+
+# Create your models here.
+
+
+class MyAccountManager(BaseUserManager):
+    def create_user(self, email, name, password=None):
+        if not email:
+            raise ValueError("User must have an email address")
+        if not name:
+            raise ValueError("User must have a name")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, password):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            password=password,
+            name=name,
+        )
+        user.is_admin=True
+        user.is_staff=True
+        user.is_superuser=True
+        user.save(using=self._db)
+        return user
+
+
+class Account(AbstractBaseUser):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(verbose_name='email', max_length=60, unique=True)
+    name = models.CharField(max_length=30, unique=True)
+    date_joined = models.DateTimeField(name='date joined', auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', ]
+
+    objects = MyAccountManager()
+
+    def __str__(self):
+        return self.email + "," + self.name
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return True
